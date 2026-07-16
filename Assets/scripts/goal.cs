381691efb8fe179ef9f8f080435b8fa00aec60cf
zsukhera <zain2466sukhera@gmail.com>
2026-07-16 11:34:27 +0500

    Revert "mistake"
    This reverts commit e78265e3cae21762c01c9c26fbf6845496b456db.

--- Assets/scripts/goal.cs
+++ Assets/scripts/goal.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class goal : MonoBehaviour
 {
-    public GameObject respawner;
+
     public GameObject audioManager;
     // Start is called before the first frame update
     void Start()
@@ -21,17 +21,6 @@ public class goal : MonoBehaviour
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("puck"))
-        {
             audioManager.GetComponent<audioManager>().playGoalSound();
-            StartCoroutine(DeactivatePuck(collision.gameObject));
-        }
     }
-
-    IEnumerator DeactivatePuck(GameObject puck)
-    {
-        yield return new WaitForSeconds(2f);
-        puck.SetActive(false);
-        respawner.GetComponent<respawnPuck>().instantiatePuckAgain();
-    }
-
 }
