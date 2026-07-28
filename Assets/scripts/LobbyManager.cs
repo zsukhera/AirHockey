3bf768c2f695ced12a6f64b1518c86935138d17c
zsukhera <zain2466sukhera@gmail.com>
2026-07-28 13:16:56 +0500

    Online Lobby

--- Assets/scripts/LobbyManager.cs
+++ Assets/scripts/LobbyManager.cs
@@ -1,18 +1,95 @@
-using System.Collections;
-using System.Collections.Generic;
+using Fusion;
+using TMPro;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class LobbyManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string currentRoomCode;
+    public GameObject waitingPanel;
+    public TMP_Text roomCodeText;
+    public TMP_InputField joinInput;
+    public static LobbyManager Instance;
+    public TMP_Text player1Text;
+    public TMP_Text player2Text;
+
+    public GameObject startButton;
+
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public async void CreateRoom()
     {
-        
+        currentRoomCode = GenerateRoomCode();
+
+        Debug.Log("Created Room Code: " + currentRoomCode);
+
+        bool success = await NetworkManager.Instance.StartGame(
+            Fusion.GameMode.Host,
+            currentRoomCode
+        );
+
+        if (success)
+        {
+            Debug.Log("Room created successfully!");
+            waitingPanel.SetActive(true);
+            roomCodeText.text = "Room Code: " + currentRoomCode;
+            startButton.GetComponent<Button>().interactable = false;
+        }
+    }
+
+    public void PlayerJoined(PlayerRef player)
+    {
+        if (player == PlayerRef.None)
+            return;
+
+        if (player.PlayerId == 1)
+        {
+            player1Text.text = "Player 1: Connected";
+        }
+        else if (player.PlayerId == 2)
+        {
+            player2Text.text = "Player 2: Connected";
+            if (NetworkManager.Instance.Runner.IsServer)
+            {
+                startButton.GetComponent<Button>().interactable = true;
+            }
+        }
+    }
+
+    public async void JoinRoom()
+    {
+        string code = joinInput.text.Trim();
+
+        if (string.IsNullOrEmpty(code))
+        {
+            Debug.Log("No room code entered");
+            return;
+        }
+
+
+        bool success = await NetworkManager.Instance.StartGame(
+            Fusion.GameMode.Client,
+            code
+        );
+        waitingPanel.SetActive(true);
+        roomCodeText.text = "";
+
+        Debug.Log("Join result: " + success);
+    }
+
+    private string GenerateRoomCode()
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        string code = "";
+
+        for (int i = 0; i < 6; i++)
+        {
+            code += chars[Random.Range(0, chars.Length)];
+        }
+
+        return code;
     }
 }
--- Assets/scripts/NetworkManager.cs
+++ Assets/scripts/NetworkManager.cs
@@ -1,8 +1,11 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Fusion;
+using Fusion.Sockets;
 using UnityEngine;
 
-public class NetworkManager : MonoBehaviour
+public class NetworkManager : MonoBehaviour, INetworkRunnerCallbacks
 {
     public static NetworkManager Instance;
     public NetworkRunner Runner;
@@ -26,7 +29,7 @@ public class NetworkManager : MonoBehaviour
         {
             Runner = gameObject.AddComponent<NetworkRunner>();
             Runner.ProvideInput = true;
-
+            Runner.AddCallbacks(this);
             Debug.Log("NetworkRunner created.");
         }
 
@@ -48,10 +51,108 @@ public class NetworkManager : MonoBehaviour
         return result.Ok;
     }
 
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+        Debug.Log($"Player left: {player}");
+    }
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        Debug.Log($"Player joined: {player}");
+
+        if (LobbyManager.Instance != null)
+        {
+            LobbyManager.Instance.PlayerJoined(player);
+        }
+    }
     public async void HostGame()
     {
         bool success = await NetworkManager.Instance.StartGame(Fusion.GameMode.Host, "TestRoom");
 
         Debug.Log("Success = " + success);
+    }
+
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnConnectedToServer(NetworkRunner runner)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnSceneLoadDone(NetworkRunner runner)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnSceneLoadStart(NetworkRunner runner)
+    {
+        throw new NotImplementedException();
     }
 }
